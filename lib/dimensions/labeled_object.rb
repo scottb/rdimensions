0a2dc6659a47f457ebfe2e821a09b139fe349a0d
Scott Brickner <scottb@brickner.net>
2010-04-03 00:11:38 -0400

    Handle missing labels more gracefully

--- lib/dimensions/labeled_object.rb
+++ lib/dimensions/labeled_object.rb
@@ -3,18 +3,20 @@ module Dimensions
     attr_reader :labels
 
     def label
-      label = @labels[ :label]
-      context = document.default_label_context
-      if label.has_key? context
-	label = label[ context]
-      else
-	label = label[ :question]
-      end
-      language = document.default_label_language
-      if label.has_key? language
-	label[ language]
-      else
-	label[ 'en-US']
+      if @labels && @labels.has_key?( :label)
+	label = @labels[ :label]
+	context = document.default_label_context
+	if label.has_key? context
+	  label = label[ context]
+	else
+	  label = label[ :question]
+	end
+	language = document.default_label_language
+	if label.has_key? language
+	  label[ language]
+	else
+	  label[ 'en-US']
+	end
       end
     end
   end
